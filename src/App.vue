362c64ae05fdea2e6895727492ceaf0373c78e78
gloriaaa0312 <3584316569@qq.com>
2024-11-04 18:01:39 +0800

    11/4

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ message }}</h1>
+  <p>{{ test.split('').reverse().join('') }}</p>
+  <div v-bind:class="a">test
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue',
+      test:'test',
+      a:'?'
+    };
+  }
+}
+</script>
